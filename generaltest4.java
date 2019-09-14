6c7eb6acfe4a9ccd8e72e8b25e0d8d9a2b6f74ba
DerrickHsu1020 <derrickhsu1020@gmail.com>
2019-09-15 00:01:02 +0100

    Update generaltest4.java

--- generaltest4.java
+++ generaltest4.java
@@ -1,11 +1,11 @@
+/*
+Write a function in a language of your choice which, when passed a decimal digit X, returns the
+value of X+XX+XXX+XXXX. E.g. if the supplied digit is 3 it should return 3702
+(3+33+333+3333).
+*/
 
 public class generaltest4 {
 	public int solution4(int x) {
 		return 1234*x;
-	}
-	public static void main(String[] args) {
-		int a='z';
-		System.out.println(a);
-		char c=97;
 	}
 }
