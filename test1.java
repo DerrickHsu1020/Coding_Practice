f1fb791d59de77c778341c7888efff55a1973ccc
DerrickHsu1020 <derrickhsu1020@gmail.com>
2019-09-15 00:03:42 +0100

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,3 +1,11 @@
+/*
+Implement (from scratch, without using Java library components) a doubly linked list (of
+integers) class which allows new integers to be added to either end of the list, any referenced
+node to be removed, and the first and last integers to be read quickly.
+
+*/
+
+
 public class test1 {
 	Node head;
 	class Node {
