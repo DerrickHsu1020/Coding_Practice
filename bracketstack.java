2e3119ac89757540e0ba1c996bc62afa7b291a03
DerrickHsu1020 <derrickhsu1020@gmail.com>
2019-09-15 00:04:32 +0100

    Update and rename test2.java to bracketstack.java

--- bracketstack.java
+++ bracketstack.java
@@ -1,3 +1,10 @@
+/*
+Write a Java function which, when passed a string containing a mixture of bracket characters -
+i.e. ‘{‘, ‘}’, ‘[‘, ‘]’, ‘(‘ and ‘)’, returns true if the brackets in the string are correctly balanced, and
+false otherwise. E.g. when called with “{[(())]()}” it should return true, but “{[(()])()}” should
+return false. Non-bracket characters in the string should be ignored.
+*/
+
 import java.util.*;
 public class test2 {
 	public boolean solution2(String s) {
